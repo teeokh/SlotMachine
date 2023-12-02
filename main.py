04c3203395e38663a8edb58adc1ed4504db47637
Tee <139244119+teeokh@users.noreply.github.com>
2023-12-02 18:05:43 +1000

    Create function to spin and print slot machine

--- main.py
+++ main.py
@@ -5,8 +5,47 @@ MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
 
-# We need the user's deposit and the user's bet
-# The user needs to enter a deposit amount until they enter a VALID amount (if they give -ve or something then no)
+ROWS = 3
+COLS = 3
+
+symbol_count = {  # Here we make a dictionary. The letters are the keys, the numbers are the values / how many times
+                  # in each col it shows
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+
+}
+
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():   # Creates symbol_count as a local variable referencing the value
+        for _ in range(symbol_count):   # For A, symbol count is 2, so we add symbol 2 times (range = 2)
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):  # This generates the number of times we need to make a column based on cols (3)
+        column = []
+        current_symbols = all_symbols[:]  # We copy the list so that we're not removing values from the actual list
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)  # Removes whatever value is generated (the symbol) from the copied list
+            column.append(value)  # Adds the value that is chosen to the column
+
+        columns.append(column)  # We then add the current column to the columns list
+
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):  # Columns is nested list, so first item will be the first singular column
+        for i, column in enumerate(columns):  # Enumerate will give the index + the value, that's why we include `i`
+            if i != len(columns) - 1:
+                print(column[row], "|")
+            else:
+                print(column[row])  # This means that we only put divider if we're not at last row of column
+
 
 def deposit():
     while True:
